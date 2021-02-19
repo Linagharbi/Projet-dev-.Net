6a371d811b4e1a4a80bc44d4949bc322b584da7b
Linagharbi <lina.gharbi@esprit.tn>
2021-02-19 14:02:04 +0100

    First commit

---
+++ PiDev.web/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PiDev.web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                "Delete",                                              // Route name
+                "{Evaluation}/{deletCritere}/{id}",                           // URL with parameters
+                new { controller = "Evaluation", action = "deletCritere", id = "200000" }  // Parameter defaults
+            );
+        }
+    }
+}
